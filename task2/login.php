4f5ea456a16dcbffb26d74168c25629ccaeaa77d
LAI YEN PING <30991538+lyping123@users.noreply.github.com>
2026-04-21 14:36:43 +0800

    Implement user authentication via database query
    Replaced hardcoded username and password validation with a database query to check user credentials.

--- task2/login.php
+++ task2/login.php
@@ -1,15 +1,15 @@
 <?php
+$conn=new mysqli("localhost","root","","db_name");
+
 session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $valid_username = "admin";
-    $valid_password = "password123";
+    $qry="SELECT * FROM users where username='$username' AND password='$password'";
+    $sttr=$conn->query($qry);
 
-    if ($username === $valid_username && $password === $valid_password) {
-        $_SESSION['loggedin'] = true;
-        $_SESSION['username'] = $username;
+    if ($sttr) {
         header("Location: welcome.php");
         exit();
     } else {
